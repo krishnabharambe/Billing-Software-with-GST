6cf8e78ee3bd0190df0e470a2b5925bd16d4bf6f
Krishna Bharambe <krishnabharambe@gmail.com>
2019-06-15 14:30:38 -0700

    added Dashboard link

---
+++ src/Dashboard/DashboardController.java
@@ -0,0 +1,23 @@
+package Dashboard;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    @FXML
+    private AnchorPane PaneDashboard;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+
+}
--- src/login/logincontroller.java
+++ src/login/logincontroller.java
@@ -2,15 +2,18 @@ package login;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
 
 import javax.management.Notification;
+import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.time.LocalDate;
@@ -20,6 +23,9 @@ import java.util.ResourceBundle;
 import static login.loginDbController.UserAuth;
 
 public class logincontroller implements Initializable {
+    @FXML
+    private AnchorPane PaneLogin;
+
     @FXML
     private TextField txtLogin;
 
@@ -39,16 +45,13 @@ public class logincontroller implements Initializable {
 
     @FXML
     void txtLoginAction(ActionEvent event) {
-            }
+    }
 
     @FXML
-    void btnLoginAction(ActionEvent event) {
+    void btnLoginAction(ActionEvent event) throws IOException {
         String username = txtLogin.getText().trim();
-        String password  = txtPassword.getText().trim();
-
-
-        if (username.isEmpty()){
-
+        String password = txtPassword.getText().trim();
+        if (username.isEmpty()) {
             //error in username
             Notifications notifications = Notifications.create();
             notifications.title("Check Username");
@@ -57,7 +60,7 @@ public class logincontroller implements Initializable {
             notifications.position(Pos.TOP_RIGHT);
             notifications.showError();
 
-        }else if (password.isEmpty()){
+        } else if (password.isEmpty()) {
             //error in password
             Notifications notifications = Notifications.create();
             notifications.title("Check Password");
@@ -65,19 +68,24 @@ public class logincontroller implements Initializable {
             notifications.hideAfter(Duration.seconds(5));
             notifications.position(Pos.TOP_RIGHT);
             notifications.showError();
-        }else{
+        } else {
             //towards the login
-             if(UserAuth(username,password)){
-                 System.out.println("Login Success");
-            }else {
-                 Notifications notifications = Notifications.create();
-                 notifications.title("Check Username/Password");
-                 notifications.text("Please, Check Your Username/Password.");
-                 notifications.hideAfter(Duration.seconds(5));
-                 notifications.position(Pos.TOP_RIGHT);
-                 notifications.showError();
-             }
-
+            if (UserAuth(username, password)) {
+                System.out.println("Login Success");
+                javafx.scene.Parent load3 = FXMLLoader.load(getClass().getResource("/Dashboard/FXMLDashboard.fxml"));
+                PaneLogin.getChildren().setAll(load3);
+                AnchorPane.setRightAnchor(load3, 0.0);
+                AnchorPane.setLeftAnchor(load3, 0.0);
+                AnchorPane.setTopAnchor(load3, 0.0);
+                AnchorPane.setBottomAnchor(load3, 0.0);
+            } else {
+                Notifications notifications = Notifications.create();
+                notifications.title("Check Username/Password");
+                notifications.text("Please, Check Your Username/Password.");
+                notifications.hideAfter(Duration.seconds(5));
+                notifications.position(Pos.TOP_RIGHT);
+                notifications.showError();
+            }
         }
     }
 }
